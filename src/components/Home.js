dbcc3e97162141b52f982abf70726b10e9500ab2
Gegardus <stgegard@gmail.com>
2022-06-14 22:59:11 +0400

    Update readme

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Home =()=> (
       <div className="flex flex-col items-center mt-12">
-      <p className="font-bold">Welcome To Hello Rails Back</p>
+      <p className="font-bold">Welcome To Hello Rails Back End</p>
       <NavLink to="/greeting" className="bg-purple-600 p-2 rounded text-slate-50 mt-4">Go for Greeting</NavLink>
     </div>
   );
